7389c5426d8434fc1dba799c928fa5b987a0f142
朱鹏轩-Studio <buding@Budings-Mac-Studio.local>
2024-03-11 22:44:22 +0800

    Adding the workflow dependencies

--- src/workflow.py
+++ src/workflow.py
@@ -4,6 +4,8 @@ import networkx as nx
 from base import Base
 from module import Module
 import numpy as np 
+import contextlib
+import os, io 
 
 class Workflow(Base):
     def __init__(self):
@@ -62,14 +64,12 @@ class Workflow(Base):
                 unresolved.append(name)
             unresolved_all.append(name)
 
-        print(unresolved_lib, unresolved)
         # resolve the library layers 
         current_layer = 1
         resolved = []
         while unresolved_lib:
             new_resolved = []
             for ii in range(len(unresolved_lib)):
-                print(unresolved_lib[ii], self.modules[unresolved_lib[ii]].required_modules)
                 if current_layer == 1:
                     if not self.modules[unresolved_lib[ii]].required_modules:
                         new_resolved.append(self.modules[unresolved_lib[ii]].name)
@@ -87,7 +87,6 @@ class Workflow(Base):
             # achieve the first layer module
             new_resolved = []
             for ii in range(len(unresolved)):
-                print(unresolved[ii], self.modules[unresolved[ii]].required_modules)
                 if current_layer == 1:
                     if not self.modules[unresolved[ii]].required_modules:
                         new_resolved.append(self.modules[unresolved[ii]].name)
@@ -97,18 +96,15 @@ class Workflow(Base):
             
             resolved.extend(new_resolved)
             unresolved = [item for item in unresolved if item not in resolved]
-            self.calc_layer[current_layer] = new_resolved
+            self.calc_layer[current_layer] = {"module": new_resolved, "ipfs": {}, "opfs": {}, "ipvs": {}, "opvs": {}, "mdls": {}}
             current_layer += 1 
         
         from pprint import pprint
-        pprint(self.libr_layer)
-        pprint(self.calc_layer)
 
     def resolve_dependencies(self):
         # Resolve dependencies based on the IOs 
         output_to_module = {} 
         for module in self.modules.values():
-            print(module.outputs)
             for output in module.outputs:
                 output_to_module[output] = module.name
 
@@ -173,73 +169,199 @@ class Workflow(Base):
                 print(f"Workflow completed successfully for sample point {sample}.")
 
     def draw_flowchart(self):
-        import matplotlib.pyplot as plt 
-        import shapely as sp 
-        import logging
-        logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
-        from matplotlib.collections import LineCollection
-        from plot import draw_logo_in_square
-        from plot import create_round_square
-        from PIL import Image
-        
-        fig = plt.figure(figsize=(10, 8))
-        ax  = fig.add_axes([0., 0., 1., 1.])
-        axlogo = fig.add_axes([0.02, 0.875, 0.08, 0.1])
-        draw_logo_in_square(axlogo)
-        ax.axis("off")
-        
-        def arraw_from_V2F(pA, pB, ax):
-            tt = np.linspace(-0.5 * np.pi, 0.4 * np.pi, 100)
-            xx = np.linspace(pA[0], pB[0], 100)
-            yy = pA[1] + (np.sin(np.sin(tt)) + 0.8414709848078965)/1.6555005931675704 * (pB[1]-pA[1])
-            points = np.array([xx, yy]).T.reshape(-1, 1, 2)
-            segments = np.concatenate([points[:-1], points[1:]], axis=1)
-            cmap = plt.get_cmap('coolwarm')  
-            norm = plt.Normalize(1.5, 5)
-            colors = cmap(norm(xx[:-1]))             
-            lc = LineCollection(segments, colors=colors, linewidth=2)
-            ax.add_collection(lc)
-            ax.plot(pA[0]-0.04, pA[1], 's', c="gray", markersize=4)
-            ax.plot(pA[0], pA[1], 's', c="darkgray", markersize=6)
-        
-        def line_from_V2M(pA, pB, ax):
-            tt = np.linspace(-0.5 * np.pi, 0.5 * np.pi, 100)
-            xx = np.linspace(pA[0], pB[0], 100)
-            yy = pA[1] + ((np.sin(np.sin(tt)) / 2 / 0.8414709848078965) + 0.5)  * (pB[1]-pA[1])
+        # print(self.libr_layer) 
+        # print(self.calc_layer)        
+        def resolve_module(module):
+            print("resolve module", module)
+            res = {"ipf": {}, "opf": {}, "ipv": {}, "opv": {}, 'ohh': 0.9, "ihh": 0.9}
+            if self.modules[module].input:
+                print("resolve ipf", module)
+                ipfl = []
+                nn = len(self.modules[module].input)
+                hh = 0.7 * nn - 0.2
+                for ii in range(nn):
+                    ipf = self.modules[module].input[ii]
+                    ipfl.append({
+                        "nam": ipf['name'],
+                        "pos": np.array([-1.1, ((nn - 1)/2 - ii) * 0.7 + 0.2]),
+                        "inc": [var for var in ipf['variables']]
+                    })
+                res["ipf"] = ipfl
+                res['ihh'] = hh 
+
+            if self.modules[module].output:
+                opfl = []
+                nn = len(self.modules[module].output)
+                hh = 0.7 * nn - 0.2 
+                h0 = 0.
+                for ii in range(nn):
+                    opf = self.modules[module].output[ii]
+                    nv = len(opf['variables'])
+                    if nv <= 2:
+                        hl = 0.5 
+                    else:
+                        hl = 0.2 * nv
+
+                    op = {
+                        "nam": opf['name'],
+                        "pos": np.array([1.1, -h0 - 0.5*hl ]),
+                        "inc": [var['name'] for var in opf['variables']]
+                    }
+                    h0 += hl 
+                    h0 += 0.1
+                    for jj in range(nv):
+                        print(jj, opf['variables'][jj])
+                        res['opv'][opf['variables'][jj]['name']] = {
+                            "pos": np.array([2.6, op["pos"][1] + ((nv-1)/2 - jj) * 0.2 ]),
+                        }
+                    opfl.append(op)
+                h0 -= 0.1
+                res["opf"] = opfl 
+                res['ohh'] = h0
+                from pprint import pprint
+            return res 
+
+        def draw_layer_module(kk, mod, ax):
+            bs = np.array([(kk-1)*7.0, 0.0])
+            oh = np.array([0., + 0.5 * mod['ohh'] - 0.2])
+
+
+            module_at_pos(bs, ax)
+            for ipf in mod['ipf']:
+                input_file_at_pos(ipf['pos'] + bs, ax)
+                line_from_F2M(ipf['pos'] + bs, bs, ax)
+            for opf in mod['opf']:
+                print(opf['pos'], oh)
+                output_file_at_pos(opf['pos'] + bs + oh, ax)
+                line_from_M2F(opf['pos'] + oh, bs, ax)
+            for op in mod['opv']:
+                pos = mod['opv'][op]['pos'] + bs + oh
+                ax.text(pos[0], pos[1], op, fontsize="small", ha="right", va="center")
+
+
+
+
+                            
+
+
+
+        with io.StringIO() as buf, contextlib.redirect_stdout(buf), contextlib.redirect_stderr(buf):
+            import matplotlib.pyplot as plt 
+            import shapely as sp 
+            import logging
+            logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
+            from matplotlib.collections import LineCollection
+            from plot import draw_logo_in_square
+            from plot import create_round_square
+            from PIL import Image
+
+            fig = plt.figure(figsize=(10, 8))
+            ax  = fig.add_axes([0., 0., 1., 1.])
+            axlogo = fig.add_axes([0.02, 0.875, 0.08, 0.1])
+            draw_logo_in_square(axlogo)
+            ax.axis("off")
+
+            def arraw_from_V2F(pA, pB, ax):
+                tt = np.linspace(-0.5 * np.pi, 0.4 * np.pi, 100)
+                xx = np.linspace(pA[0], pB[0], 100)
+                yy = pA[1] + (np.sin(np.sin(tt)) + 0.8414709848078965)/1.6555005931675704 * (pB[1]-pA[1])
+                points = np.array([xx, yy]).T.reshape(-1, 1, 2)
+                segments = np.concatenate([points[:-1], points[1:]], axis=1)
+                cmap = plt.get_cmap('coolwarm')  
+                norm = plt.Normalize(-1.5, 1.5)
+                colors = cmap(norm(tt[:-1]))             
+                lc = LineCollection(segments, colors=colors, linewidth=2)
+                ax.add_collection(lc)
+                ax.plot(pA[0]-0.04, pA[1], 's', c="gray", markersize=4)
+                ax.plot(pA[0], pA[1], 's', c="darkgray", markersize=6)
+
+            def line_from_V2M(pA, pB, ax):
+                tt = np.linspace(-0.5 * np.pi, 0.5 * np.pi, 100)
+                xx = np.linspace(pA[0], pB[0], 100)
+                yy = pA[1] + ((np.sin(np.sin(tt)) / 2 / 0.8414709848078965) + 0.5)  * (pB[1]-pA[1])
+
+                ax.plot(xx, yy, "-", c="#3b4dc0", lw=2)
+
+            def line_from_M2F(pF, pM, ax):
+                tt = np.linspace(0., 0.5*np.pi, 100)
+                xx = np.linspace(0.43 + pM[0], pF[0] + pM[0], 100 )
+                yy = pM[1] - 0.2 + (np.sin(np.sin(tt))) / 0.8414709848078965 * (pF[1] + 0.2)
+                ax.plot(xx, yy, "-", c="#6a97ff", lw=3)
+
+            def line_from_F2M(pF, pM, ax):
+                tt = np.linspace(-0.5*np.pi, 0., 100)
+                xx = np.linspace(pF[0], pM[0] -0.44, 100 )
+                yy = pM[1] + 0.2 + (np.sin(np.sin(tt))) / 0.8414709848078965 * (pF[1] - 0.2)
+                ax.plot(xx, yy, "-", c="#6a97ff", lw=3)
+
+            def input_file_at_pos(pos, ax):
+                image_path = "src/icons/inputfile.png"  
+                image = Image.open(image_path)
+                image = np.array(image)
+                ax.imshow(image, extent=[pos[0]-0.5, pos[0], pos[1]-0.25, pos[1]+0.25], zorder=100)
+
+            def output_file_at_pos(pos, ax):
+                image_path = "src/icons/outputfile.png"  
+                image = Image.open(image_path)
+                image = np.array(image)
+                ax.imshow(image, extent=[pos[0], pos[0]+0.5, pos[1]-0.25, pos[1]+0.25], zorder=100)
+
+            def module_at_pos(pos, ax):
+                image_path = "src/icons/calculator.png"  
+                image = Image.open(image_path)
+                image = np.array(image)
+                ax.imshow(image, extent=[pos[0]-0.45, pos[0]+0.45, pos[1]-0.45, pos[1]+0.45], zorder=100)
+
+            def lib_at_pos(pos, ax):
+                image_path = "src/icons/library.png"  
+                image = Image.open(image_path)
+                image = np.array(image)
+                ax.imshow(image, extent=[pos[0]-0.45, pos[0]+0.45, pos[1]-0.45, pos[1]+0.45], zorder=100)
+
+            def sampler_at_pos(pos, ax):
+                image_path = "src/icons/sampler.png"  
+                image = Image.open(image_path)
+                image = np.array(image)
+                ax.imshow(image, extent=[pos[0]-0.45, pos[0]+0.45, pos[1]-0.45, pos[1]+0.45], zorder=100)
+
             
-            ax.plot(xx, yy, "-", c="#3b4dc0", lw=2)
-        
-        def input_file_at_pos(pos, ax):
-            image_path = "src/icons/inputfile.png"  
-            image = Image.open(image_path)
-            image = np.array(image)
-            ax.imshow(image, extent=[pos[0]-0.5, pos[0], pos[1]-0.25, pos[1]+0.25])
+            outframe = create_round_square([2, 4,])
+            x_coords = [point[0] for point in outframe.exterior.coords]
+            y_coords = [point[1] for point in outframe.exterior.coords]  
+            # ax.plot(x_coords, y_coords, '-', color='grey', lw=0.3)
+            # ax.fill(x_coords, y_coords, fc='#072348', ec=None)
+            ax.set_xlim([0, 10])
+            ax.set_ylim([-4, 4])
 
-        
-        def module_at_pos(pos, ax):
-            image_path = "src/icons/calculator.png"  
-            image = Image.open(image_path)
-            image = np.array(image)
-            ax.imshow(image, extent=[pos[0]-0.65, pos[0]+0.65, pos[1]-0.65, pos[1]+0.65])
-        
-        outframe = create_round_square([2, 4,])
-        x_coords = [point[0] for point in outframe.exterior.coords]
-        y_coords = [point[1] for point in outframe.exterior.coords]  
-        # ax.plot(x_coords, y_coords, '-', color='grey', lw=0.3)
-        # ax.fill(x_coords, y_coords, fc='#072348', ec=None)
-        ax.set_xlim([0, 10])
-        ax.set_ylim([0, 8])
-        arraw_from_V2F([1., 1.], [4., 3], ax)
-        arraw_from_V2F([1., 1.3], [4., 3], ax)
-        line_from_V2M([1., 1.3], [4.5, 7], ax)
-        input_file_at_pos([4.5, 3], ax)
-        module_at_pos([5.4, 2.7], ax)
-        
+            lib_at_pos([1, 6], ax)
+            sampler_at_pos([1, 3], ax)
+            arraw_from_V2F([2., 1.], [4., 3], ax)
+            arraw_from_V2F([2., 1.3], [4., 3], ax)
+            line_from_V2M([2., 1.3], [4.5, 7], ax)
+            input_file_at_pos([4.8, 3], ax)
+            module_at_pos([5.4, 2.8], ax)
+            output_file_at_pos([6.0, 2.6], ax)
+            ax.text(7, 2.6, r"{mn1}", ha="right", va="center")
+            ax.text(7, 2.3, r"{mn1}", ha="right", va="center")
+            ax.text(7, 2.05, r"{mn1}", ha="right", va="center")
+            ax.text(7, 1.85, r"{mn1}", ha="right", va="center")
+
+        # print(self.calc_layer)
+        # for kk, item in self.calc_layer.items():
+        #     for mdl in item['mdls']:
+        #         module_at_pos(item['mdls'][mdl]['pos'], ax)
+
+        #     for opf in item['opfs']:
+        #         # print(opf, item)
+        #         output_file_at_pos(item["opfs"][opf]['pos'], ax)
+
+        for kk, item in self.calc_layer.items():
+            for module in item['module']:
+                mod = resolve_module(module)
+                # print(mod)
+                draw_layer_module(kk, mod, ax)
 
 
-        
 
-        
-              
-        # plt.show()
-        plt.savefig("flowchart.png", dpi=300)
+        plt.show()
+            # plt.savefig("flowchart.png", dpi=300)
