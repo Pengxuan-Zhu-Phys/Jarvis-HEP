fe7ed3f6b848f1012f388dbdfec69508f77f0d4b
Pengxuan@Alien-R12 <zhupx99@icloud.com>
2024-04-05 00:22:48 +0800

    Jarvis-HEP V2.0.0-beta ready

--- src/Sampling Source/adaptive-0.13.1/adaptive/_static_version.py
+++
@@ -1,2 +0,0 @@
-# This file has been created by setup.py.
-version = '0.13.1'
--- src/Sampling Source/adaptive-0.13.1/adaptive/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Union
-
-import numpy as np
-
-Float = Union[float, np.float_]
-Int = Union[int, np.int_]
-Real = Union[Float, Int]
